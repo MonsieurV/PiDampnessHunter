60627a896357646fc93090f068741e3e15d1c7d6
Yoan Tournade <yoan@ytotech.com>
2016-01-08 20:15:58 +0100

    This at least work

--- app.py
+++ app.py
@@ -26,28 +26,32 @@ MAX_TEMPERATURE = 21.5
 TICK = 10
 # As a multiple of TICK.
 MIN_DURATION = 3
-min_duration_counter = 3
+
+class HeatStrategy:
+	def __init__(self):
+		self.min_duration_counter = 3
+
+	def heat(self, temperature, humidity):
+		if self.min_duration_counter > 0:
+			return self.continue_heating()
+		if temperature > THRESHOLD_TEMPERATURE and humidity < THRESHOLD_HUMIDITY:
+			return stop_heating()
+		if temperature > MAX_TEMPERATURE:
+			return self.stop_heating()
+		# TODO Check the current duration of heating: if the heater has been working
+		# more than MAX_DURATION, make a pause during PAUSE_DURATION.
+		return True
+
+	def stop_heating(self):
+		self.min_duration_counter = MIN_DURATION
+		return False
+
+	def continue_heating(self):
+		self.min_duration_counter = self.min_duration_counter - 1
+		return True
 
 pifacedigital = pifacedigitalio.PiFaceDigital()
-
-def heat(temperature, humidity):
-	if min_duration_counter > 0:
-		return continue_heating()
-	if temperature > THRESHOLD_TEMPERATURE and humidity < THRESHOLD_HUMIDITY:
-		return stop_heating()
-	if temperature > MAX_TEMPERATURE:
-		return stop_heating()
-	# TODO Check the current duration of heating: if the heater has been working
-	# more than MAX_DURATION, make a pause during PAUSE_DURATION.
-	return True
-
-def stop_heating():
-	min_duration_counter = MIN_DURATION
-	return False
-
-def continue_heating():
-	min_duration_counter = min_duration_counter - 1
-	return True
+strategy = HeatStrategy()
 
 def quit_gracefully(*args):
     pifacedigital.relays[1].turn_off()
@@ -61,7 +65,7 @@ try:
 			continue
 		print('Temperature: {0:0.1f}°C -- Humidity: {1:0.1f}%'.format(
 			temperature, humidity))
-		if heat(temperature, humidity):
+		if strategy.heat(temperature, humidity):
 			# Turn on the heater.
 			pifacedigital.relays[1].turn_on()
 		else:
