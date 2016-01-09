ea0032c3d657b8ec0f84d0bc115d798210c45d67
Yoan Tournade <yoan@ytotech.com>
2016-01-09 11:54:43 +0100

    This at least work

--- app.py
+++ app.py
@@ -20,35 +20,39 @@ import signal
 
 # TODO Conceive a smarter algorithm that use 'target' values,
 # not raw thresholds.
-THRESHOLD_HUMIDITY = 48
+THRESHOLD_HUMIDITY = 48.5
 THRESHOLD_TEMPERATURE = 18.5
 MAX_TEMPERATURE = 21.5
+# In seconds.
 TICK = 10
 # As a multiple of TICK.
 MIN_DURATION = 3
 
 class HeatStrategy:
 	def __init__(self):
-		self.min_duration_counter = 3
+		self.duration_counter = None
 
 	def heat(self, temperature, humidity):
-		if self.min_duration_counter > 0:
-			return self.continue_heating()
+		if self.duration_counter is not None and self.duration_counter < MIN_DURATION:
+			return self.start_heating()
 		if temperature > THRESHOLD_TEMPERATURE and humidity < THRESHOLD_HUMIDITY:
 			return self.stop_heating()
 		if temperature > MAX_TEMPERATURE:
 			return self.stop_heating()
 		# TODO Check the current duration of heating: if the heater has been working
 		# more than MAX_DURATION, make a pause during PAUSE_DURATION.
+		return self.start_heating()
+
+	def start_heating(self):
+		if self.duration_counter is None:
+			self.duration_counter = 0
+		else:
+			self.duration_counter = self.duration_counter + 1
 		return True
 
 	def stop_heating(self):
-		self.min_duration_counter = MIN_DURATION
+		self.duration_counter = None
 		return False
-
-	def continue_heating(self):
-		self.min_duration_counter = self.min_duration_counter - 1
-		return True
 
 pifacedigital = pifacedigitalio.PiFaceDigital()
 strategy = HeatStrategy()
